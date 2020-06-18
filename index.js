ad50e0dcef81af350b529938e1382bb740e58b73
LucasFCdev <59072990+LucasFCdev@users.noreply.github.com>
2020-06-18 08:12:57 -0300

    create aula

---
+++ index.js
@@ -0,0 +1,7 @@
+const nomeDaVariavelOrConstante = "um valor" // variavel do tipo string o valor pode ser passdo por: "" ou '' ou ´´ 
+
+const nome = "Mayk"
+const nome2 = 'Diego'
+const notaAluno01 = 9.8 
+
+console.log(typeof notaAluno01)
